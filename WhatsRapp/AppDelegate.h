ed7a17b606a398972e80d8f1fa4dcbdb35469439
ITSAdmin <ITSAdmin@cw4305mac41.local>
2017-11-21 10:57:43 +0800

    Initial Commit

---
+++ WhatsRapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatsRapp
+//
+//  Created by ITSAdmin on 21/11/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatsRapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatsRapp
+//
+//  Created by ITSAdmin on 21/11/17.
+//  Copyright © 2017 ITE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
